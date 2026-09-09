3ed6273457998395d3878556e30489b7f9ecb7d1
Shubham Rajput <rajput.shubh1993@gmail.com>
2026-09-10 00:40:53 +0530

    Constant Time

---
+++ Big-O/constant_time.py
@@ -0,0 +1,11 @@
+## O(1) — Constant Time
+
+# O(1) means the algorithm performs a constant amount of work, 
+# regardless of the input size.
+# O(1) is considered one of the most efficient time complexities because 
+# the operation does not depend on the input size.
+
+def get_first(numbers):
+  return numbers[0]
+
+print(get_first([10, 3, 5, 7])) # 10
