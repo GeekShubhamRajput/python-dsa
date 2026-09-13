186c816ed01cca12214292fe80fa874bfd67dad7
Shubham Rajput <rajput.shubh1993@gmail.com>
2026-09-13 23:44:21 +0530

    Update append

--- linked-list/append.py
+++ linked-list/append.py
@@ -5,7 +5,7 @@ class Node:
 
 class LinkedList:
   def __init__(self, value):
-    new_node = Node(2)
+    new_node = Node(value)
     self.head = new_node
     self.tail = new_node
     self.length = 1
